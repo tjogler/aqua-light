cfc2cc5028f455ac9ccd10a9fb632020cfd8729b
tjogler <tjogler@users.noreply.github.com>
2016-02-20 21:28:25 +0100

    Added config file option to easily provide the lamp geometry, example file with the proper format x,y,lednames is config.py

---
+++ config_2squars.py
@@ -0,0 +1,4 @@
+x=-14,-10.,-6.,-2.,2,6.,10.,14,-14.,-10.,-6.,-2.,2,6.,10,14,-14,-10.,-6.,-2.,2,6.,10,14
+y=-11,-11.,-11.,-8.,-8.,-8.,-4.,-4.,-4.,0.,0.,0.,4.,4.,4.,8.,8.,8.,12.,12.,12.,-14.,-14.,-14
+leds=rbLed,wLed,rbLed,wLed,rbLed,wLed,rbLed,wLed,rbLed,wLed,rbLed,wLed,rbLed,wLed,rbLed,wLed,rbLed,wLed,rbLed,wLed,rbLed,wLed,rbLed,wLed
+
