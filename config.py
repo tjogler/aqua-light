99863454c4a64072b8abc97a24a93f561d7ddf25
tjogler <tjogler@users.noreply.github.com>
2016-02-23 13:20:39 +0100

    example config file with for led position definition and led types

---
+++ config.py
@@ -0,0 +1,4 @@
+x=-8,-6,-4,-2,0,2,4,6,8,-8,-6,-4,-2,0,2,4,6,8
+y=-5.,-5.,-5.,-5.,-5.,-5.,-5.,-5.,-5.,5.,5.,5.,5.,5.,5.,5.,5.,5.
+leds=rbLed,wLed,rbLed,wLed,rbLed,wLed,rbLed,wLed,rbLed,wLed,rbLed,wLed,rbLed,wLed,rbLed,wLed,rbLed,wLed
+
