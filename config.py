2f8f163a439213e8b977cd8a2aae724184ebfe2e
tjogler <tjogler@users.noreply.github.com>
2016-03-04 20:05:14 +0100

    updated config file to comply with shadowing function example is for two led lamps

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 x=-8,-6,-4,-2,0,2,4,6,8,-8,-6,-4,-2,0,2,4,6,8
 y=-5.,-5.,-5.,-5.,-5.,-5.,-5.,-5.,-5.,5.,5.,5.,5.,5.,5.,5.,5.,5.
 leds=rbLed,wLed,rbLed,wLed,rbLed,wLed,rbLed,wLed,rbLed,wLed,rbLed,wLed,rbLed,wLed,rbLed,wLed,rbLed,wLed
-
+angle=180.
+lamp=0,-5.,20,20,2.1,1.3;0,5,20,20,2.1,1.3
